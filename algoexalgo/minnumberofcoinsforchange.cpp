66e5ede0b15083bf7e1fe1557d7ef4fb99ec5816
Shekhar0050M <shekhar0050m@gmail.com>
2021-07-19 17:10:09 +0530

    al1-30cmp170919

--- algoexalgo/minnumberofcoinsforchange.cpp
+++ algoexalgo/minnumberofcoinsforchange.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 // My method
+//Never use
 inline bool leastcoins(int denominations[],int amount,int currentsum,int n,vector<int> &coins,vector<int> &coinnum){
     if(currentsum==amount){
         coinnum.push_back(coins.size());
@@ -22,9 +23,23 @@ inline bool leastcoins(int denominations[],int amount,int currentsum,int n,vecto
     return 0;
 }
 
+inline int algoguymethod(int denominations[],int n,int amount){
+    sort(denominations,denominations+n);
+    vector<int> ways(amount+1);
+    for(int i=0;i<n;i++){
+        for(int j=denominations[i];j<=amount;j++){
+            ways[j]=ways[j-denominations[i]]+1;
+        }
+    }
+    return ways[amount];
+}
+
 int main(){
     int denominations[]={1,2,4},amount=6,n=sizeof(denominations)/sizeof(denominations[0]);
     vector<int> ways,coinnum;
-    leastcoins(denominations,amount,0,n,ways,coinnum);
-    printf("Minimum coins can be of :: %d",*min_element(coinnum.begin(),coinnum.end()));
+    //leastcoins(denominations,amount,0,n,ways,coinnum);
+    //printf("Minimum coins can be of :: %d\n",*min_element(coinnum.begin(),coinnum.end()));
+
+
+    printf("Minimum coins by algo guy:: %d",algoguymethod(denominations,n,amount));
 }
